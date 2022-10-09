d9f69cacf67253f36bd1cc411af092bc8d020e24
Binod Bhatt <95048143+binodbhatt@users.noreply.github.com>
2022-10-09 15:39:33 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hey, welcome to my website")
+
+menuIcon.addEventListener('click', () => {
+  nav.style.display = 'block';
+});
+
+
+closeNav.addEventListener('click', () => {
+  nav.style.display = 'none';
+});
